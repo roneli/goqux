17903a8a967948c354efafe7585b94df848bd3f8
Ron Eliahu <38083777+roneli@users.noreply.github.com>
2023-09-10 10:20:58 +0300

    support encode zero values

--- struct_test.go
+++ struct_test.go
@@ -11,10 +11,11 @@ import (
 
 func TestEncodeValues(t *testing.T) {
 	tableTests := []struct {
-		name     string
-		model    interface{}
-		values   map[string]SQLValuer
-		skipFlag string
+		name           string
+		model          interface{}
+		values         map[string]SQLValuer
+		skipFlag       string
+		skipZeroValues bool
 	}{
 		{
 			name: "encode_insert",
@@ -23,21 +24,44 @@ func TestEncodeValues(t *testing.T) {
 				unexported  bool
 				FieldToSkip int `goqux:"skip_insert"`
 			}{IntField: 1},
-			values:   map[string]SQLValuer{"int_field": {1}},
-			skipFlag: skipInsert,
+			values:         map[string]SQLValuer{"int_field": {1}},
+			skipFlag:       skipInsert,
+			skipZeroValues: false,
 		},
 		{
 			name: "encode_array",
 			model: struct {
 				IntField []int
 			}{IntField: []int{1, 2}},
-			values:   map[string]SQLValuer{"int_field": {[]int{1, 2}}},
-			skipFlag: skipInsert,
+			values:         map[string]SQLValuer{"int_field": {[]int{1, 2}}},
+			skipFlag:       skipInsert,
+			skipZeroValues: false,
+		},
+		{
+			name: "encode_array_with_zero_values",
+			model: struct {
+				IntField []int
+			}{IntField: []int{1, 0}},
+			values:         map[string]SQLValuer{"int_field": {[]int{1, 0}}},
+			skipFlag:       skipInsert,
+			skipZeroValues: false,
+		},
+		{
+			name: "encode_zero_values",
+			model: struct {
+				IntField    int
+				FloatField  float64
+				unexported  bool
+				FieldToSkip int `goqux:"skip_insert"`
+			}{IntField: 0},
+			values:         map[string]SQLValuer{},
+			skipFlag:       skipInsert,
+			skipZeroValues: true,
 		},
 	}
 	for _, tt := range tableTests {
 		t.Run(tt.name, func(t *testing.T) {
-			values := encodeValues(tt.model, tt.skipFlag)
+			values := encodeValues(tt.model, tt.skipFlag, tt.skipZeroValues)
 			assert.Equal(t, tt.values, values)
 		})
 	}
@@ -50,7 +74,7 @@ func TestEncodeTimeValue(t *testing.T) {
 		FieldToSkip int `goqux:"skip_insert"`
 	}{
 		FieldToSkip: 5,
-	}, skipInsert)
+	}, skipInsert, true)
 	if tf, ok := values["time_field"]; ok {
 		require.NotNil(t, tf)
 		return
